df005ed8f157ea2eb996fc9684105c2c70d51b7e
Matías Berríos <matiasberrios@uc.cl>
2022-12-20 13:43:44 -0300

    add placeholder eng & esp buttons

--- javascript/scrollveal.js
+++ javascript/scrollveal.js
@@ -68,8 +68,20 @@ ScrollReveal().reveal(".contact-wrapper", {
   delay: 600,
 });
 
-ScrollReveal().reveal(".fa", {
-  ...defaultProps,
-  delay: 500,
-  interval: 100,
-});
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
