afde168cfd643925d7656934e8de29ba487e41fb
m5b6 😽 <77901615+m5b6@users.noreply.github.com>
2024-08-25 13:04:49 -0400

    made simple

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"rPhU3ys3qDnpqHD53nQPllswwEwwiUUm7SYvEn8MNxs=\"}"
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm run build
-git add -f out
-git commit -m "Deploying build to gh-pages"
-git push -f origin `git subtree split --prefix out HEAD`:refs/heads/gh-pages
